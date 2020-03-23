e981c1cf063b180b5fceda651c944bbd876542a5
Taylor Roebuck <taylornroebuck@gmail.com>
2020-03-22 19:22:55 -0700

    added styling for dark mode

--- README.md
+++ README.md
@@ -59,5 +59,5 @@ Your finished project must include all of the following requirements:
 
 ## Stretch Problems
 
-- [ ] Implement another custom hook
+- [X] Implement another custom hook
 - [ ] Use a graphing library to graph the data you received from the API
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Player from './components/Player';
+import Nav from './components/Nav';
 
 class App extends React.Component {
   constructor() {
@@ -26,6 +27,7 @@ class App extends React.Component {
     render(){
       return (
         <div className="App">
+          <Nav />
           <div className="player-container" data-testid='players'>
             {this.state.players.map(players => {
               return <Player key={players.id} players={players} />
