fadc11d5e9172becccb82629ae4777da0a2646e2
Taylor Roebuck <taylornroebuck@gmail.com>
2020-03-22 18:30:31 -0700

    fetched data from API and displayed the data on the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Player from './components/Player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:5000/api/players')
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        this.setState( { players: response } )
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+    render(){
+      return (
+        <div className="App">
+          <div className="player-container" data-testid='players'>
+            {this.state.players.map(players => {
+              return <Player key={players.id} players={players} />
+            })}
+          </div>
+        </div>
+      )
+    }
 }
 
 export default App;
